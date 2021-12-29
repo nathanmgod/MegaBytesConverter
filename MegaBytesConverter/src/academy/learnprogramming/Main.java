ad711ac2f3ddfcb910243ebd69406c9facefc60f
Nathan de Moura Godoy <nathanmgodoy@gmail.com>
2021-12-29 09:38:01 -0300

    Add files via upload

---
+++ MegaBytesConverter/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MegaBytesandKiloBytesCalculator.printMegaBytesAndKiloBytes(2500);
+        MegaBytesandKiloBytesCalculator.printMegaBytesAndKiloBytes(-1024);
+        MegaBytesandKiloBytesCalculator.printMegaBytesAndKiloBytes(5000);
+
+    }
+}
